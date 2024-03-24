9761b0538fd92b9973efcd3393da4ee0cb2ceaa5
AlCo2 <abdou.lahboub.wwe@gmail.com>
2024-03-24 02:24:48 +0000

    change init and add sqlalchemy

--- models/base_model.py
+++ models/base_model.py
@@ -2,35 +2,46 @@
 """This module defines a base class for all models in our hbnb clone"""
 import uuid
 from datetime import datetime
+import sqlalchemy
+from sqlalchemy import Column, String, DateTime
+from sqlalchemy.ext.declarative import declarative_base
 from models import storage
 
 time = "%Y-%m-%dT%H:%M:%S.%f"
 
+if models.storage_t == "db":
+    Base = declarative_base()
+else:
+    Base = object
+
+
 class BaseModel:
     """A base class for all hbnb models"""
-    def __init__(self, *args, **kwargs):
-        """Instatntiates a new model"""
-        if not kwargs:
-            self.id = str(uuid.uuid4())
-            self.created_at = datetime.now()
-            self.updated_at = datetime.now()
-            storage.new(self)
-        else:
-            for key, value in kwargs.items():
-                if key != '__class__':
-                    setattr(self, key, value)
+    if models.storage_t == "db":
+        id = Column(String(60), primary_key=True)
+        created_at = Column(DateTime, default=datetime.utcnow)
+        updated_at = Column(DateTime, default=datetime.utcnow)
 
-            if kwargs.get('id', None) is None:
-                self.id = str(uuid.uuid4())
+    def __init__(self, *args, **kwargs):
+        """ init method for BaseMode """
+        if kwargs:
+            for key, value in kwargs.items():
+                if key != "__class__":
+                    setattr(self, key, value)
             if kwargs.get("created_at", None) and type(self.created_at) is str:
-                self.created_at = datetime.strptime(kwargs['created_at'], time)
+                self.created_at = datetime.strptime(kwargs["created_at"], time)
             else:
-                self.created_at = datetime.now()
-            if kwargs.get("updated_at", None) and type(self.created_at) is str:
-                self.updated_at = datetime.strptime(kwargs['updated_at'], time)
+                self.created_at = datetime.utcnow()
+            if kwargs.get("updated_at", None) and type(self.updated_at) is str:
+                self.updated_at = datetime.strptime(kwargs["updated_at"], time)
             else:
-                self.updated_at = datetime.now()
-            storage.new(self)
+                self.updated_at = datetime.utcnow()
+            if kwargs.get("id", None) is None:
+                self.id = str(uuid.uuid4())
+        else:
+            self.id = str(uuid.uuid4())
+            self.created_at = datetime.utcnow()
+            self.updated_at = self.created_at
 
     def __str__(self):
         """Returns a string representation of the instance"""
