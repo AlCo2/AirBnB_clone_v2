17997f21049a91b5a60628f684f77e9bc772037a
AlCo2 <abdou.lahboub.wwe@gmail.com>
2024-04-04 18:45:43 +0000

    fix error

--- 2-do_deploy_web_static.py
+++ 2-do_deploy_web_static.py
@@ -15,11 +15,12 @@ def do_deploy(archive_path):
         file = archive_path.split("/")[-1]
         name = file.split(".")[0]
         path = "/data/web_static/releases/"
-        put(archive_path, "/tmp/{}".format(name))
-        run('mkdir -p {}'.format(path))
-        run("tar -xzf /tmp/{} -C {}/{}".format(archive_path, path, name))
-        run('rm -rf /data/web_static/current/*')
+        put(archive_path, "/tmp/")
+        run('mkdir -p {}{}'.format(path, name))
+        run("tar -xzf /tmp/{} -C {}{}/".format(file, path, name))
+        run("rm /tmp/{}".format(file))
         run("mv {}{}/web_static/* {}{}".format(path, name, path, name))
+        run('rm -rf /data/web_static/current/')
         run('ln -s {}{} /data/web_static/current'.format(path, name))
         return True
     except Exception:
