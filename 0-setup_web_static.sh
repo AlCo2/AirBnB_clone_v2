bbc7bb09db9c0a6b64eca2429b5dbdbd5c6b67be
AlCo2 <abdou.lahboub.wwe@gmail.com>
2024-04-04 01:14:16 +0000

    add chgrp

--- 0-setup_web_static.sh
+++ 0-setup_web_static.sh
@@ -6,7 +6,9 @@ mkdir -p /data/web_static/releases/test/
 mkdir -p /data/web_static/shared/
 echo "test file" > /data/web_static/releases/test/index.html
 ln -sf /data/web_static/releases/test/ /data/web_static/current
-chown -R ubuntu ubuntu /data/
+
+chown -R ubuntu /data/
+chgrp -R ubuntu /data/
 
 printf %s "server {
     listen 80 default_server;
