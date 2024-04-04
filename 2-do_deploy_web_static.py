fd3b31cd05dd551767de6e93ebc94b3a09f7aad4
AlCo2 <abdou.lahboub.wwe@gmail.com>
2024-04-04 18:38:34 +0000

    remove unecessary function

--- 2-do_deploy_web_static.py
+++ 2-do_deploy_web_static.py
@@ -1,27 +1,9 @@
 #!/usr/bin/python3
 """ deploy a web app to servers """
 import os.path
-import time
-from fabric.api import local
-from fabric.api import env
-from fabric.api import put
-from fabric.api import run
+from fabric.api import env, run, put
 
 env.hosts = ["100.25.143.95", "54.175.165.203"]
-
-
-def do_pack():
-    """ the function to create the tgz """
-    t = time.localtime()
-    current_time = time.strftime("%Y%m%d%H%M%S", t)
-    name = "versions/web_static_{}.tgz".format(current_time)
-
-    if os.path.isdir("versions") is False:
-        if local("mkdir -p versions").failed is True:
-            return None
-    if local("tar -cvzf {} web_static".format(name)).failed is True:
-        return None
-    return name
 
 
 def do_deploy(archive_path):
