7b21a46f35175771fdc8ec54dd17d4cdf02c241a
AlCo2 <abdou.lahboub.wwe@gmail.com>
2024-04-04 18:15:52 +0000

    add try catch

--- 2-do_deploy_web_static.py
+++ 2-do_deploy_web_static.py
@@ -9,6 +9,7 @@ from fabric.api import run
 
 env.hosts = ["100.25.143.95", "54.175.165.203"]
 
+
 def do_pack():
     """ the function to create the tgz """
     t = time.localtime()
@@ -22,20 +23,22 @@ def do_pack():
         return None
     return name
 
+
 def do_deploy(archive_path):
     """ function to deployt the app """
 
     if os.path.isfile(archive_path) is False:
         return False
-
-    file = archive_path.split("/")[-1]
-    name = file.split(".")[0]
-    path = "/data/web_static/releases/"
-
-    put(archive_path, "/tmp/{}".format(name))
-    run('mkdir -p {}'.format(path))
-    run("tar -xzf /tmp/{} -C {}/{}".format(archive_path, path, name))
-    run ('rm -rf /data/web_static/current/*')
-    run("mv {}{}/web_static/* {}{}".format(path, name, path ,name))
-    run('ln -s {}{} /data/web_static/current'.format(path, name))
-    return True
+    try:
+        file = archive_path.split("/")[-1]
+        name = file.split(".")[0]
+        path = "/data/web_static/releases/"
+        put(archive_path, "/tmp/{}".format(name))
+        run('mkdir -p {}'.format(path))
+        run("tar -xzf /tmp/{} -C {}/{}".format(archive_path, path, name))
+        run('rm -rf /data/web_static/current/*')
+        run("mv {}{}/web_static/* {}{}".format(path, name, path, name))
+        run('ln -s {}{} /data/web_static/current'.format(path, name))
+        return True
+    except Exception:
+        return False
