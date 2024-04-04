95b60f51dbc6a92b585b727fbd2d499488a21bf3
AlCo2 <abdou.lahboub.wwe@gmail.com>
2024-04-04 01:44:51 +0000

    task 2

---
+++ 1-pack_web_static.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+""" fabric script to generates a tgz archive """
+import os.path
+import time
+from fabric.api import local
+
+def do_pack():
+    """ the function to create the tgz """
+    t = time.localtime()
+    current_time = time.strftime("%Y%m%d%H%M%S", t)
+    name = "versions/web_static_{}.tgz".format(current_time)
+    local("mkdir -p versions")
+    local("tar -cvcf {} web_static".format(name))
+    return name
