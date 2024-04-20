3e606f5ca0777ec94c2364cc146c71ad7abc9838
AlCo2 <abdou.lahboub.wwe@gmail.com>
2024-04-20 23:35:04 +0100

    fix doc

--- web_flask/0-hello_route.py
+++ web_flask/0-hello_route.py
@@ -1,12 +1,13 @@
 #!/usr/bin/python3
-# script to run flask app
+""" script to run flask app """
 from flask import Flask
 
 app = Flask(__name__)
 
 @app.route("/")
 def index():
+    """ handle route / """
     return "Hello HBNB!"
 
 if __name__ == '__main__':
-   app.run()
+   app.run(host='0.0.0.0', port='5000')
