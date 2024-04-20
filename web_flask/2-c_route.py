6df9c8b26eee644256a1efcd71779a48fe8e5ef7
AlCo2 <abdou.lahboub.wwe@gmail.com>
2024-04-20 23:48:45 +0100

    fix task 2 logic erro

--- web_flask/2-c_route.py
+++ web_flask/2-c_route.py
@@ -20,7 +20,7 @@ def hbnb():
 @app.route('/c/<text>', strict_slashes=False)
 def c_isfum(text):
     """ handle route /c/<text> """
-    return "C {}".format(text)
+    return "C {}".format(text.replace('_', " "))
 
 
 if __name__ == '__main__':
