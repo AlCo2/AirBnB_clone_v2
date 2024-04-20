0bec78429658495c15c40618ecc748ba4a04b8be
AlCo2 <abdou.lahboub.wwe@gmail.com>
2024-04-20 23:54:27 +0100

    add handle for /python route

--- web_flask/3-python_route.py
+++ web_flask/3-python_route.py
@@ -23,6 +23,7 @@ def c_isfun(text):
     return "C {}".format(text.replace('_', " "))
 
 
+@app.route('/python', strict_slashes=False)
 @app.route("/python/<text>", strict_slashes=False)
 def python(text="cool"):
     """ handle route /python/<text> """
